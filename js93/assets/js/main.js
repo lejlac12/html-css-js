f8d72919f2b6b8f30bb08073416702962dabf5f5
lejlac12 <76840973+lejlac12@users.noreply.github.com>
2021-02-19 18:33:55 +0100

    Pop-up nastavak

---
+++ js93/assets/js/main.js
@@ -0,0 +1,45 @@
+function hidePopup() {
+	let popup = document.getElementById('gallery-preview');
+	popup.classList.remove('visible');
+}
+
+function init() {
+	galleryItems();
+	bindKeyDown();
+}
+
+function galleryItems() {
+	let images = document.querySelectorAll('.gallery a');
+	
+	for (let image of images) {
+		image.addEventListener('click', showImage);
+	}
+}
+
+function showImage(event) {
+	event.preventDefault();
+	
+	let link = event.target.parentElement.href;
+	
+	let image = document.querySelector('#gallery-preview img');
+	
+	image.src = link;
+	
+	let popup = document.getElementById('gallery-preview');
+	
+	popup.classList.add('visible');
+}
+
+function bindKeyDown() {
+	window.addEventListener('keydown', keyDownHandler);
+}
+
+function keyDownHandler(event) {
+	switch (event.key) {
+		case "Escape":
+			hidePopup();
+			break;
+	}
+}
+
+window.addEventListener('load', init);
